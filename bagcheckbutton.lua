038ca16fad587a1cde4d1132b8cf91eb925a073c
Rhoda Baker <rhoda.baker@gmail.com>
2012-12-15 14:24:55 -0500

    Added pretty IndyIcon and removed the fuglybutton

--- bagcheckbutton.lua
+++ bagcheckbutton.lua
@@ -1,15 +1,37 @@
 local context = UI.CreateContext("Indy_BagContext")
 
-local bagButtonFrame = UI.CreateFrame("Frame", "Indy_BagButton", context)
-bagButtonFrame:SetVisible(false)
+local bagButtonTexture2 = UI.CreateFrame("Texture", "Indy_BagButtonTexture2", context)
+bagButtonTexture2:SetTexture("Indy", "textures/IndyIcon_Normal.png")
+bagButtonTexture2:SetPoint("BOTTOMRIGHT", UI.Native.Bag, "TOPLEFT", 15, 15)
+bagButtonTexture2:SetVisible(false)
 
-local bagButtonTexture1 = UI.CreateFrame("Texture", "Indy_BagButtonTexture1", bagButtonFrame)
-bagButtonTexture1:SetTexture("Indy", "textures/fuglybutton.png")
-bagButtonTexture1:SetPoint("BOTTOMRIGHT", UI.Native.Bag, "TOPLEFT", 15, 15)
+function bagButtonTexture2.Event:MouseIn()
+    bagButtonTexture2:SetTexture("Indy", "textures/IndyIcon_Over.png")
+end
 
-local bagButtonTexture2 = UI.CreateFrame("Texture", "Indy_BagButtonTexture2", bagButtonTexture1)
-bagButtonTexture2:SetTexture("Indy", "textures/3x3_grid_icon&16.png")
-bagButtonTexture2:SetPoint("CENTERCENTER",bagButtonTexture1, "CENTERCENTER")
+function bagButtonTexture2.Event.MouseOut()
+    bagButtonTexture2:SetTexture("Indy", "textures/IndyIcon_Normal.png")
+end
+
+function bagButtonTexture2.Event.LeftDown()
+    bagButtonTexture2:SetTexture("Indy", "textures/IndyIcon_Down.png")
+end
+
+function bagButtonTexture2.Event.LeftUp()
+    bagButtonTexture2:SetTexture("Indy", "textures/IndyIcon_Over.png")
+end
+
+function bagButtonTexture2.Event.RightDown()
+    bagButtonTexture2:SetTexture("Indy", "textures/IndyIcon_Down.png")
+end
+
+function bagButtonTexture2.Event.RightUp()
+    bagButtonTexture2:SetTexture("Indy", "textures/IndyIcon_Over.png")
+end
+
+function bagButtonTexture2.Event.MouseOut()
+    bagButtonTexture2:SetTexture("Indy", "textures/IndyIcon_Normal.png")
+end
 
 bagButtonTexture2.Event.LeftClick = function()
     Indy:CheckBagsForArtifacts()
@@ -25,8 +47,8 @@ tt:InjectEvents(bagButtonTexture2, function() return ttString end)
 
 function Indy:ShowBagCheckButton()
     if Indy.showBagCheckButton then
-        bagButtonFrame:SetVisible(true)
+        bagButtonTexture2:SetVisible(true)
     else
-        bagButtonFrame:SetVisible(false)
+        bagButtonTexture2:SetVisible(false)
     end
 end
