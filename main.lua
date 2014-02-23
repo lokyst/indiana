b66564e27f212a23dc01b3e9733268cf6d4f2b7f
Rhoda Baker <rhoda.baker@gmail.com>
2014-02-23 11:39:04 -0500

    Move strsplit and strtrim functions into the private namespace so they can be accessed from any file.

--- main.lua
+++ main.lua
@@ -1,32 +1,8 @@
+local addon, private = ...
+local strsplit = private.strsplit
+local strtrim = private.strtrim
+
 Indy = {}
-
--- Some useful generic functions
-local function strsplit(delimiter, text)
-    local list = {}
-    local pos = 1
-    if string.find("", delimiter, 1) then -- this would result in endless loops
-        error("delimiter matches empty string!")
-    end
-    while 1 do
-        local first, last = string.find(text, delimiter, pos)
-        if first then -- found?
-            table.insert(list, string.sub(text, pos, first-1))
-            pos = last+1
-        else
-            table.insert(list, string.sub(text, pos))
-            break
-        end
-    end
-    return list
-end
-
-local function strtrim(s)
-    if not s then
-        return nil
-    end
-
-    return s:match'^%s*(.*%S)' or ''
-end
 
 -- Define default values. Default value must never be nil
 
