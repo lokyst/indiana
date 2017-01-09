531bea34727cbaf4c3f4f02d791f93c269f8a071
Rhoda Baker <rhoda.baker@gmail.com>
2017-01-09 10:16:07 -0500

    Fixed possible bug in co-routine cputime measurement and improved logging.

--- bulkartifactquery.lua
+++ bulkartifactquery.lua
@@ -103,54 +103,52 @@ local function ArtifactNameQuery()
     print("Starting artifact name query")
     local failedItemQueries = {}
 
-    local itemCounter = 1
-    local cycleCounter = 1
+    local itemCounter = 0
+    local succeededItems = 0
     local failedItems = 0
     local cpuStartTime = Inspect.Time.Real()
+    local loopCpuStartTime = Inspect.Time.Real()
 
-    for itemId, _ in pairs(Indy.unnamedArtifacts) do
+    for itemId, _ in pairs(Indy.artifactTable) do
         local success
-        local failCounter = 1
+        local failCounter = 0
         local loopCpuTime
-        local loopCpuStartTime = Inspect.Time.Real()
+        
         repeat
-            --if cycleCounter % 1 == 0 then
-                --local startTime = Inspect.Time.Real()
-                success, itemDetail = pcall(Inspect.Item.Detail, itemId)
-                --print("ProcessTime: " .. (Inspect.Time.Real() - startTime))
+            success, itemDetail = pcall(Inspect.Item.Detail, itemId)
 
-                if success then
-                    Indy.newList[itemId].name =  itemDetail.name
-                    Indy.unnamedArtifacts[itemId] = nil
+            if success then
+                --Indy.newList[itemId].name =  itemDetail.name
+                --Indy.unnamedArtifacts[itemId] = nil
 
-                    itemCounter = itemCounter + 1
-                else
+                succeededItems = succeededItems + 1
+            else
 
-                    failCounter = failCounter + 1
-                end
+                failCounter = failCounter + 1
+            end
 
-                if failCounter > 50 then
-                    failedItemQueries[itemId] = true
-                    failedItems = failedItems + 1
-                    success = true
-                end
+            -- if a particular item fails to query more than 50 times quit trying.
+            if failCounter > 50 then
+                failedItemQueries[itemId] = true
+                failedItems = failedItems + 1
+                success = true
+            end
 
-                loopCpuTime = Inspect.Time.Real() - loopCpuStartTime
-                --print("Cycles processed: ".. cycleCounter .. " Items Processed: " .. itemCounter .. " Failed: " .. failedItems .. " Loop CPUTime: " .. loopCpuTime)
-            --end
+            -- Increment item counter after we have determined success or failure so that itemCounter = successCounter + failCounter
+            itemCounter = itemCounter + 1
 
-
+            -- Co routine check happens inside repeat because we might need to query same item multiple times until it succeeds
+            loopCpuTime = Inspect.Time.Real() - loopCpuStartTime            
             if loopCpuTime > 0.01 then
                 coroutine.yield(false)
                 loopCpuStartTime =  Inspect.Time.Real()
             end
 
-            --[[if cycleCounter % 1 == 0 then
-                coroutine.yield(false)
-            end--]]
-
-            cycleCounter = cycleCounter + 1
         until success
+        
+        if itemCounter % 100 == 0 then 
+            print("[BulkArtifactQuery] Items Processed: " .. itemCounter .. " Success: " .. succeededItems .. " Failed: " .. failedItems .. " Total CPUTime: " .. Inspect.Time.Real() - cpuStartTime)
+        end
     end
 
     Indy.artifactTableCount = itemCounter
@@ -158,7 +156,8 @@ local function ArtifactNameQuery()
     Indy.failedItemQueries = failedItemQueries
     Indy.cpuTime = Inspect.Time.Real() - cpuStartTime
 
-    print("Artifact name query complete")
+    print("[BulkArtifactQuery] Items Processed: " .. itemCounter .. " Success: " .. succeededItems .. " Failed: " .. failedItems)    
+    print("[BulkArtifactQuery] Query complete after " .. Indy.cpuTime .. " seconds")
     return true
 end
 
